f699df8f51f3918117e97ebbd8d0d2289727596f
YogenderSha <yogender.sharma@anzocontrols.com>
2024-05-30 15:30:46 +0530

    first commit

---
+++ src/main/java/com/example/tinyurlclonev/controller/RedirectController.java
@@ -0,0 +1,39 @@
+package com.example.tinyurlclonev.controller;
+
+import com.example.tinyurlclonev.entity.Redirect;
+import com.example.tinyurlclonev.request.RedirectCreationRequest;
+import com.example.tinyurlclonev.service.RedirectService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import jakarta.validation.Valid;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import static org.springframework.http.HttpStatus.MOVED_PERMANENTLY;
+
+@RestController
+public class RedirectController {
+
+    private RedirectService redirectService;
+
+    public RedirectController(RedirectService redirectService) {
+        this.redirectService = redirectService;
+    }
+
+    @GetMapping("/{alias}")
+    public ResponseEntity<?> handleRedirect(@PathVariable String alias) throws URISyntaxException {
+        Redirect redirect = redirectService.getRedirect(alias);
+        System.out.println("We're redirecting here!" + redirect);
+        URI uri = new URI(redirect.getUrl());
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setLocation(uri);
+        return new ResponseEntity<>(httpHeaders, MOVED_PERMANENTLY);
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<?> createRedirect(@Valid @RequestBody RedirectCreationRequest redirectCreationRequest) {
+        return ResponseEntity.ok(redirectService.createRedirect(redirectCreationRequest));
+    }
+}
